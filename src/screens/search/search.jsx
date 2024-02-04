34af87a0da730a65459217a715f62ae0885b36c0
Seoheeda <1015aliciay@gmail.com>
2024-02-05 01:23:49 +0900

    menu details layout

--- src/screens/search/search.jsx
+++ src/screens/search/search.jsx
@@ -10,56 +10,5 @@ import {
 
 export default function Menu() {
 
-    const [inputText, setInputText] = useState('');
-
-    const handleInputChange = (text) => {
-        setInputText(text);
-    };
-
-    const handleButtonPress = () => {
-        Alert.alert('You entered: ' + inputText);
-    };
-
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.searchcontainer}>
-                <TextInput
-                    style={styles.searchbox}
-                    placeholder="검색어를 입력하세요."
-                    onChangeText={handleInputChange}
-                    value={inputText}
-                />
-                <Text style={styles.canceltext}>취소</Text>
-            </View>
-        </View>
-    )
-        ;
-}
-
-const styles = StyleSheet.create({
-    container: {
-        width: "100%",
-        backgroundColor: "#fff",
-        marginBottom: 10,
-    },
-    searchcontainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
-    searchbox: {
-        height: 40,
-        backgroundColor: "#f0f0f0",
-        borderRadius: 10,
-        width: "80%",
-        margin: 10,
-        padding: 5,
-    },
-    canceltext: {
-        fontSize: 15,
-        width: 40,
-        height: 40,
-        marginTop: 20,
-        marginHorizontal: 10,
-    },
-});
+    return <Text>검색</Text>
+};
