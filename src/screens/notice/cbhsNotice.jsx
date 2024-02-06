fcd65581eeca387434337fb6e23bd4f73ba9f6b2
박유진 <qkrdbw@naver.com>
2024-02-06 23:58:09 +0900

    feat: add noticebox component in CbhsNotice

--- src/screens/notice/cbhsNotice.jsx
+++ src/screens/notice/cbhsNotice.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { View } from "react-native";
-import Pagination from '../../components/notice/pagination';
 import Noticebox from '../../components/notice/noticebox';
 
 export default function CbhsNotice() {
   return (
     <View >
       <Noticebox/>
-      <Pagination
-        totalPages={totalPages}
-        currentPage={currentPage}
-        onPageChange={onPageChange}
-      />
     </View>
   );
 }
