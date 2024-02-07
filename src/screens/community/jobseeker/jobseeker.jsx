10543c1fdd035978cfde33ac8996c0e60169b8a7
박유진 <qkrdbw@naver.com>
2024-02-07 16:57:54 +0900

    feat: board component

--- src/screens/community/jobseeker/jobseeker.jsx
+++ src/screens/community/jobseeker/jobseeker.jsx
@@ -1,5 +1,8 @@
-import { Text } from "react-native";
+import React from 'react';
+import Board from '../../../components/board/board'
 
 export default function Jobseeker() {
-  return <Text>Jobseeker Posts</Text>;
+  return (
+    <Board/>
+  );
 }
