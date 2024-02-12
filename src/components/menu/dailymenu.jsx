6e9fa3ec8d8a4769a11c0a9fe46ecd168f48dedd
Seoheeda <1015aliciay@gmail.com>
2024-02-12 15:13:56 +0900

    Comment: 주석 해제

--- src/components/menu/dailymenu.jsx
+++ src/components/menu/dailymenu.jsx
@@ -15,7 +15,6 @@ export default function RecentPostCard({ selectedDate }) {
         const day = originalDate.getUTCDate().toString().padStart(2, '0');
         const month = (originalDate.getUTCMonth() + 1).toString().padStart(2, '0');
         const year = originalDate.getUTCFullYear().toString().slice(2, 4);
-        console.log(originalDate)
       
         return year + month + day;
       }
