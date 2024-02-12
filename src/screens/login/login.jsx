e451454f054282ef7d14bb532d3fa10f3090f659
Minhyung Cho <dup33333@hufs.ac.kr>
2024-02-13 00:30:00 +0900

    Design: login screen Design

--- src/screens/login/login.jsx
+++ src/screens/login/login.jsx
@@ -56,9 +56,9 @@ export default function LoginScreen({ navigation }) {
           <Image source={logo} style={{ width: 200, height: 200 }} />
           <Text style={styles.logoText}>돔토리</Text>
           <Text style={{ fontSize: 15, marginBottom: 20, fontWeight: 700 }}>
-            돔토리는 충북학사생 전용 커뮤니티 서비스입니다.
+            충북학사생 전용 커뮤니티 서비스
           </Text>
-          <View styles={styles.inputWrapper}>
+          <View style={styles.inputWrapper}>
             <TextInput
               autoCorrect={false}
               spellCheck={false}
@@ -130,10 +130,10 @@ const styles = StyleSheet.create({
     padding: 20,
     textAlign: "center",
     flexDirection: "column",
-    gap: 30,
+    gap: 10,
   },
   loginFormTextInput: {
-    width: 350,
+    width: "90%",
     height: 60,
     fontSize: 20,
     borderRadius: 15,
@@ -141,26 +141,27 @@ const styles = StyleSheet.create({
     borderColor: "orange",
     backgroundColor: "#fafafa",
     paddingLeft: 10,
-    marginTop: 10,
     marginBottom: 10,
   },
   loginButton: {
     color: "orange",
     backgroundColor: "orange",
     borderRadius: 15,
-    width: 350,
+    width: "80%",
     height: 60,
     marginTop: 10,
     alignItems: "center",
     justifyContent: "center",
   },
   signupButton: {
-    width: 200,
+    width: "60%",
     height: 45,
     marginTop: 30,
     backgroundColor: "#fff5d3",
     alignItems: "center",
     justifyContent: "center",
+    borderColor: "orange",
+    borderWidth: 1,
     borderRadius: 15,
   },
 });
