c7b382eb4ae95741ed73b2f67457262f6f0aa1f0
박유진 <qkrdbw@naver.com>
2024-02-17 02:50:24 +0900

    feat: general

--- src/screens/community/general/general.jsx
+++ src/screens/community/general/general.jsx
@@ -2,13 +2,8 @@ import React from "react";
 import Board from "../../../components/board/board";
 import PostDetail from "../../../components/board/postDetail";
 
-<<<<<<< HEAD
 export default function General({navigation}) {
   return (
     <Board boardId={1} navigation={navigation}/>
   );
-=======
-export default function General() {
-  return <Board boardId="1" />;
->>>>>>> origin/master
 }
