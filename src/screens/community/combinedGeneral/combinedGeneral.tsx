bef3b76870871c55d0314eb78f5c0deb0000c98c
Minhyung Cho <dup33333@hufs.ac.kr>
2024-09-01 16:13:37 +0900

    feat: combinedGeneral boards

---
+++ src/screens/community/combinedGeneral/combinedGeneral.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Board from "../../../components/board/board";
+import PostDetail from "../../../components/board/postDetail";
+
+export default function CombinedGeneral({ navigation }) {
+  return <Board boardId={7} navigation={navigation} />;
+}
